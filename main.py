ae9749d4bdee6826d15bb4564da15caf8e6a4823
jinanmh123 <147313181+jinanmh123@users.noreply.github.com>
2023-12-03 16:20:07 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,65 @@
+import scrapy
+import json
+import httpx
+import asyncio
+
+class MySpider(scrapy.Spider):
+    name = 'my_spider'
+    start_url = 'https://fooddelivery.mykeeta.com/api/v1/homePage/homePageInfo'
+
+    # The Headers (same as the given 'requests' library script)
+    headers = {
+        'deviceType': 'sdk_gphone64_arm64',
+        'Accept-Language': 'en',
+        'appVersion': '1.1.10',
+        'retrofit_exec_time': '1700590724587',
+        'timeZone': 'GMT+08:00',
+        'M-SHARK-TRACEID': '5171f71126b2d4544b38a98a4ff0e4cd66d7a16999806706990672227d03c17005907245875e1c22',
+        'cityId': '810001',
+        'locale': 'en',
+        'uuid': '00000000000007024EEFCC47C49358643C0AE382C498BA169998067154711603',
+        'platform': '4',
+        'clientType': 'c_android',
+        'districtId': '8100019',
+        'osVersion': '33_13',
+        'partner': '85204',
+        'seq_id': '13',
+        'region': 'HK',
+        'request_id': '28BAC42F-6E16-4DFD-B978-EE48F2BEA1AF',
+        'Content-Type': 'application/json; charset=UTF-8',
+        'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 13; sdk_gphone64_arm64 Build/TE1A.220922.034)'
+    }
+
+    # The body/main data (same as in the given 'requests' library script)
+    form_data = {
+        "location": {
+            "latitude": "22.31924733900611",
+            "longitude": "114.1692228242755"
+        }
+    }
+
+    # make the post request asynchronously to increase speed
+    async def make_request(self):
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                self.start_url,
+                headers=self.headers,
+                data=json.dumps(self.form_data),
+            )
+
+            # Save the response to output.js file
+            with open('output.json', 'wb') as f:
+                f.write(response.content)
+
+    # start the request
+    def start_requests(self):
+        loop = asyncio.get_event_loop()
+        yield from loop.run_until_complete(self.make_request())
+
+    def parse(self, response):
+        print(response.body)
+
+# run the spider
+if __name__ == '__main__':
+    from scrapy import cmdline
+    cmdline.execute(['python','-m','scrapy', 'runspider', 'main.py'])
